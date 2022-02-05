c465c0164fa3612ed9212d55bf4cf620a2367d97
wtfaq <d.cizov@live.de>
2022-02-05 11:18:54 +0100

    add purgecss and watch script

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,19 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+
 module.exports = {
-	plugins: [require("tailwindcss"), require("autoprefixer")],
+	plugins: [
+		require("tailwindcss"),
+		process.env.NODE_ENV === "production" ? require("autoprefixer") : null,
+		process.env.NODE_ENV === "production"
+			? cssnano({ preset: "default" })
+			: null,
+		purgecss({
+			content: [
+				"./views/**/*.pug",
+			],
+			defaultExtractor: (content) =>
+				content.match(/[\w-/:]+(?<!:)/g) || [],
+		}),
+	],
 };
